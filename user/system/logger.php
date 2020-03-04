20073d400222b53f312ceac9c4840ebaa633f5b5
zukardex <zenzenram@gmail.com>
2020-03-05 02:45:04 +0530

    Update logger.php

--- user/system/logger.php
+++ user/system/logger.php
@@ -2,11 +2,11 @@
 
 
 
-{"Username":"Suhail","Password":"2003","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1; rv:58.0) Gecko\/20100101 Firefox\/58.0","Time":1521443647}
+{"Username":"Suhail","Password":"....","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1; rv:58.0) Gecko\/20100101 Firefox\/58.0","Time":1521443647}
 {"Username":"Suhail","Password":"-4","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1; rv:58.0) Gecko\/20100101 Firefox\/58.0","Time":1521443742}
-{"Username":"Suhail","Password":"2003","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1; rv:58.0) Gecko\/20100101 Firefox\/58.0","Time":1521444395}
-{"Username":"Admin","Password":"2003","IP":"192.168.43.1","UA":"Mozilla\/5.0 (Linux; Android 6.0; iris870 Build\/MRA58K) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/64.0.3282.137 Mobile Safari\/537.36","Time":1521457108}
-{"Username":"Admin","Password":"2003","IP":"192.168.43.1","UA":"Mozilla\/5.0 (X11; Linux x86_64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/37.0.2049.0 Safari\/537.36","Time":1521457924}
-{"Username":"Admin","Password":"2003","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1; rv:58.0) Gecko\/20100101 Firefox\/58.0","Time":1521458263}
+{"Username":"Suhail","Password":"test","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1; rv:58.0) Gecko\/20100101 Firefox\/58.0","Time":1521444395}
+{"Username":"Admin","Password":"nz","IP":"192.168.43.1","UA":"Mozilla\/5.0 (Linux; Android 6.0; iris870 Build\/MRA58K) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/64.0.3282.137 Mobile Safari\/537.36","Time":1521457108}
+{"Username":"Admin","Password":"bla8","IP":"192.168.43.1","UA":"Mozilla\/5.0 (X11; Linux x86_64) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/37.0.2049.0 Safari\/537.36","Time":1521457924}
+{"Username":"Admin","Password":"3333","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1; rv:58.0) Gecko\/20100101 Firefox\/58.0","Time":1521458263}
 {"Username":"Admin","Password":"1","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/64.0.3282.186 Safari\/537.36","Time":1521472274}
 {"Username":"User","Password":"10","IP":"::1","UA":"Mozilla\/5.0 (Windows NT 6.1) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/64.0.3282.186 Safari\/537.36","Time":1521472305}
